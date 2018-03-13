4fdfbb065c937431f81faa9a35e91667191dca60
Bartek <gekarez@gmail.com>
2018-03-13 15:35:59 +0100

    Initial commit

---
+++ src/main/java/com/elektroniks/SpringBootMvcElectronicShopApplication.java
@@ -0,0 +1,12 @@
+package com.elektroniks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcElectronicShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcElectronicShopApplication.class, args);
+	}
+}
